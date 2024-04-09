ecdfdcd35fdb25f5c1a8e28ea08b23e25ac8465e
Tristan Kirscher <85068746+Kirscher@users.noreply.github.com>
2024-04-09 20:51:53 +0000

    [+] added ROC curve plotting function

--- utils/classification_report.py
+++ utils/classification_report.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn.metrics import classification_report, confusion_matrix, ConfusionMatrixDisplay
+from sklearn.metrics import classification_report, confusion_matrix, ConfusionMatrixDisplay, roc_curve, auc
 from sklearn.inspection import permutation_importance
 
 def plot_confusion_matrix(model, y_test, y_pred):
@@ -25,7 +25,7 @@ def plot_feature_importance(model, X_test, y_test, n_repeats=10, random_state=42
 
     # create a DataFrame to store the feature importance
     feature_importance = pd.DataFrame({
-        'feature': X.columns,
+        'feature': X_test.columns,
         'importance_mean': perm_importance['importances_mean'],
         'importance_std': perm_importance['importances_std']
     })
@@ -42,3 +42,22 @@ def plot_feature_importance(model, X_test, y_test, n_repeats=10, random_state=42
     plt.ylabel('Feature')
     plt.show()
 
+def plot_ROC_curve(model, X_test, y_test):
+    '''
+    This function calculates the ROC curve and plots it.
+    '''
+    # calculate the ROC curve
+    y_score = model.predict_proba(X_test)
+    fpr, tpr, _ = roc_curve(y_test, y_score[:,1])
+
+    # plot the ROC curve
+    plt.figure(figsize=(8, 6))
+    plt.plot(fpr, tpr, label='ROC curve (area = %0.2f)' % auc(fpr, tpr))
+    plt.plot([0, 1], [0, 1], 'k--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver Operating Characteristic')
+    plt.legend(loc="lower right")
+    plt.show()
